76c46fccdd4d82a9b340d4570de063e6dbde505b
Tanya Rabadjiyska <t.rabadjiyska@yahoo.com>
2018-03-26 18:20:22 +0300

    Hw1

--- Problem_1.R
+++ Problem_1.R
@@ -1,1 +1,30 @@
+#Problem 1 [Vectors and arithmetic operations, sequences]
 
+# (a)
+x<-c(4,1,1,4)
+
+# (b)
+y<-c(1,4)
+
+# (c)
+x-y
+
+#(d)
+s<-c(x,y)
+
+#(e)
+rep(s, 10)
+length(rep(s,10))
+
+#(f)
+rep(s, each=3)
+
+#(g)
+#i
+seq(7,21,by=1)
+
+#ii
+t<-7:21
+
+#(h)
+length(t)
--- Problem_2.R
+++ Problem_2.R
@@ -1,1 +1,19 @@
+#Problem 2  [Vectors, data frames, subsetting]
 
+#(a)
+date<-c('03Mon18', '04Tue18', '05Wed18', '04Thu18', '05Fri18', '06Sat18', '07Sun18')
+xmin<-c(23.0, 20.5, 28.2, 20.3, 22.4, 17.2, 18.2)
+xmax<-c(25.0, 22.8, 31.2, 27.3, 28.4, 20.2, 24.1)
+
+#(b)
+xmax-xmin
+
+#(c)
+avgxmin<-mean(xmin)
+avgxmax<-mean(xmax)
+
+#(d)
+xmin[xmin<avgxmin]
+
+#(e)
+xmin[xmax>avgxmax]
